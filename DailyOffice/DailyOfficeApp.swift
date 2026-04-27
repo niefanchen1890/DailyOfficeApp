80be0c087fea22cbb0f96e8e8374bca0b9cd0d01
“niefanchen1890” <kechengyun@gmail.com>
2026-04-27 16:06:20 +0800

    Initial Commit

---
+++ DailyOffice/DailyOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyOfficeApp.swift
+//  DailyOffice
+//
+//  Created by Anthony Lim on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
